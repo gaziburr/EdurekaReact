055c68e5393515bd91d3d09fae7ffe858dd014ae
Hiroyuki Xiong <xionghiroyuki@gmail.com>
2019-09-06 15:03:02 -0700

    Pushing updates for later continuation

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Grab check from Scott',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Apply to a job',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Learn up more on tech skills',
+        completed: false
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hello World</h1>
-        <Todos/>
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 class Todos extends Component {
   render() {
-    return (
-        <h2>I am learning React!</h2>
-    );
+    return this.props.todos.map((todo) => (
+        <h3>{ todo.title }</h3>
+    ));
   }
 }
 
