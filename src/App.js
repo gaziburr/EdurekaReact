80cb5dd49e83fcfd7b60f81b4eb7334f8de7a3cc
Hiroyuki Xiong <xionghiroyuki@gmail.com>
2019-09-06 14:13:54 -0700

    Change react format to follow class components similar to angular

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <Todos/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+  render() {
+    return (
+        <h2>I am learning React!</h2>
+    );
+  }
+}
+
+export default Todos;
